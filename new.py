cc9faf407e475521ca84e8be6ffa65970651bdcd
linjun.li <linjun.li@yingzt.com>
2017-06-15 17:07:25 +0800

    sb

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-//自定义求绝对值
+# 自定义求绝对值
 def my_abs(x):
     if not isinstance(x, (int, float)):
         raise TypeError('bad operand type')
@@ -7,14 +7,14 @@ def my_abs(x):
     else:
         return -x
 
-//求阶层
+# 求阶层
 def fact(n):
     if n == 1:
         return 1
     return n * fact(n - 1)
 print(fact(100))
 
-//汉诺塔
+# 汉诺塔
 step = []
 def move(n, a='A', b='B', c='C'):
     if n == 1:
@@ -24,11 +24,48 @@ def move(n, a='A', b='B', c='C'):
         move(n-1, a, c, b)
         move(1, a, b, c)
         move(n-1, b, a, c)
-N = int(input('step num:'))
-move(N)
+# N = input('step num:')
+# if not isinstance(N, (int,float)):
+#     raise TypeError('input type only int or float')
+# move(int(N))
+move(5)
 print("need",len(step),step)
 
-//python切片功能slice
+# python切片功能slice
 L = ['A', 'B', 'C', 'D', 'E']
+print('abcdefg'[:3])
 print(L[1:3])
 print(L[-2:-1])
+
+# 特殊迭代
+d = {'a': 1,'b': 2, 'c': 3}
+for key in d:
+    print('key = ',key)
+
+for value in d.values():
+    print('value = ',value)
+
+for key,value in d.items():
+    print('key = ',key,' value = ',value)
+
+for x,y in [(1,1), (2,4), (3,8), (4,16)]:
+    print(x,y)
+
+# 列表生成式
+listRange = [x * x for x in range(1,11)]
+print(listRange)
+listPai = [m + n for m in 'ABC' for n in 'XYZ']
+print(listPai)
+isStr = isinstance('xyz', str)
+print(isStr)
+
+# 生成器generator
+# 列表生成器list
+Lis = [x * x for x in range(10)]
+print(Lis)
+# generator
+genera = (x * x for x in range(10))
+print(genera)
+for n in genera:
+    print(n)
+
